c77bc16c643f1d4b321ca55a4e80fdd3905a4a35
root <dahirmuhammad3@gmail.com>
2018-04-28 08:59:55 +0100

    added regex exercise

---
+++ python/Excercises/regexExercise/regexExercise.py
@@ -0,0 +1,43 @@
+#codeName: regexExercise.py
+#Author: Dahir Muhammad Dahir
+#Date: 28th-March-2018
+#About: regular expressions exercises
+
+import re
+
+
+def main():
+    #exercise1()
+    exercise2()
+
+
+def exercise1():
+    while(True):
+        validWords = re.compile("[bh][aiu]t")
+        word = raw_input("Enter a valid:\n")
+        if validWords.match(word):
+            print("%s is a valid word"%(word))
+        elif word == "exit":
+            break
+        else:
+            print("Invalid word")
+
+
+def exercise2():
+    while(True):
+        userName = re.compile("[A-Z][a-z]{1,}\s{1,1}[A-Z][a-z]{1,}")
+        name = raw_input("Enter your username:\n")
+        if userName.match(name):
+            print("%s is a valid userName"%(name))
+        elif name == "exit":
+            break
+        else:
+            print("invalid userName")
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
