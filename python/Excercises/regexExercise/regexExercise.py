83fcd0d35986f9f652d7e86b5e4289ddd764d35d
Ethic41 <dahirmuhammad3@gmail.com>
2018-05-06 13:50:09 +0100

    added exercise4 and 5

--- python/Excercises/regexExercise/regexExercise.py
+++ python/Excercises/regexExercise/regexExercise.py
@@ -9,7 +9,9 @@ import re
 def main():
     #exercise1()
     #exercise2()
-    exercise3()
+    #exercise3()
+    #exercise4()
+    exercise5()
 
 
 def exercise1():
@@ -47,6 +49,28 @@ def exercise3():
         else:
             print("invalid userName")
 
+def exercise4():
+    while(True):
+        validIdentifier = re.compile(r"^[A-Za-z_][A-Za-z0-9_]{1,}\b")
+        identifier = raw_input("Enter an identifier:\n")
+        if validIdentifier.match(identifier):
+            print("%s is a valid python identifier"%(identifier))
+        elif identifier == "exit":
+            break
+        else:
+            print("invalid python identifier")
+
+
+def exercise5():
+    while(True):
+        validStreetAdress = re.compile("^[0-9]{1,} {1,1}([A-Z]{1,1}[a-z]{1,} {0,1}){1,}\b")
+        validStreet = raw_input("Enter a street address:\n")
+        if validStreetAdress.match(validStreet):
+            print("%s is a valid Street address"%(validStreet))
+        elif validStreet == "exit":
+            break
+        else:
+            print("invalid street address")
 
 
 if __name__=="__main__":
