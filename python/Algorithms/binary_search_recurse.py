7dce4629094e4d48d09e63a5c7c31c18d810b847
root <dahirmuhammad3@gmail.com>
2018-05-14 08:49:05 +0100

    style fix

--- python/Algorithms/binary_search_recurse.py
+++ python/Algorithms/binary_search_recurse.py
@@ -1,24 +1,27 @@
 def main():
     a = raw_input("a:")
     a = a.split(",")
-    print a
+    print(a)
     x = input("x:")
     r = len(a)
     p = 1
-    recurse(a,p,r,x)
-def recurse(a,p,r,x):
+    recurse(a, p, r, x)
+
+
+def recurse(a, p, r, x):
     if p > r:
         return "Not found"
     else:
         q = (p+r)/2
-        print q,x,p,r
+        print(q, x, p, r)
 
-        if int(a[q])==x:
+        if int(a[q]) == x:
             return q
-        elif int(a[q])>x:
+        elif int(a[q]) > x:
             return recurse(a, p, q-1, x)
-        elif int(a[q])<x:
+        elif int(a[q]) < x:
             return recurse(a, q+1, r, x)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print(main())
